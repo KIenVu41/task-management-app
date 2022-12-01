a53e4e426457678bd7440a799a8fe9cc8d0eb6a5
kienvt4120 <kienvutrung20@gmail.com>
2022-12-01 17:21:42 +0700

    fix otp layout

--- TaskManagement/app/src/main/java/com/kma/taskmanagement/ui/dialog/OTPVerificationDialog.java
+++ TaskManagement/app/src/main/java/com/kma/taskmanagement/ui/dialog/OTPVerificationDialog.java
@@ -15,19 +15,21 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.kma.taskmanagement.R;
+import com.kma.taskmanagement.TaskApplication;
 import com.kma.taskmanagement.utils.GlobalInfor;
 
 public class OTPVerificationDialog extends Dialog {
 
-    private EditText otp1, otp2, otp3, otp4;
-    private TextView tvResend, tvOtpEmail;
+    private EditText otp1, otp2, otp3, otp4, otp5, otp6, edtOtpPhone;
+    private TextView tvResend, tvSend;
     private Button btnVerify;
     private boolean resendEnabled = false;
-    private int resendTime = 60;
+    private int resendTime = 30;
     private int selected = 0;
 
     public OTPVerificationDialog(@NonNull Context context) {
@@ -46,20 +48,30 @@ public class OTPVerificationDialog extends Dialog {
         otp2 = findViewById(R.id.otp2);
         otp3 = findViewById(R.id.otp3);
         otp4 = findViewById(R.id.otp4);
+        otp5 = findViewById(R.id.otp5);
+        otp6 = findViewById(R.id.otp6);
+        edtOtpPhone = findViewById(R.id.edtOtpPhone);
+
         tvResend = findViewById(R.id.tvResend);
-        tvOtpEmail = findViewById(R.id.tvOtpEmail);
+        tvSend = findViewById(R.id.tvSend);
         btnVerify = findViewById(R.id.btnVerify);
 
         otp1.addTextChangedListener(textWatcher);
         otp2.addTextChangedListener(textWatcher);
         otp3.addTextChangedListener(textWatcher);
         otp4.addTextChangedListener(textWatcher);
-        if(GlobalInfor.email.length() != 0) {
-            tvOtpEmail.setText(GlobalInfor.email);
-        }
         showKeyBoard(otp1);
 
-        startCountDownTime();
+        tvSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edtOtpPhone.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getContext(), "Chưa nhập sđt", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                startCountDownTime();
+            }
+        });
 
         tvResend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,7 +87,12 @@ public class OTPVerificationDialog extends Dialog {
             public void onClick(View view) {
                 final String getOtp = otp1.getText().toString()+otp2.getText().toString()+otp3.getText().toString()+otp4.getText().toString();
 
-                if(getOtp.length() == 4) {
+                if(resendEnabled) {
+                    Toast.makeText(getContext(), "Mã hết hạn, vui lòng nhập mã mới", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(getOtp.length() == 6) {
 
                 }
             }
@@ -105,6 +122,12 @@ public class OTPVerificationDialog extends Dialog {
                 } else if(selected == 2) {
                     selected = 3;
                     showKeyBoard(otp4);
+                } else if(selected == 3) {
+                    selected = 4;
+                    showKeyBoard(otp5);
+                } else if(selected == 4) {
+                    selected = 5;
+                    showKeyBoard(otp6);
                 } else {
                     btnVerify.setBackgroundColor(R.drawable.round_back_red);
                 }
@@ -123,6 +146,7 @@ public class OTPVerificationDialog extends Dialog {
     private void startCountDownTime() {
         resendEnabled = false;
         tvResend.setTextColor(Color.parseColor("#99000000"));
+        btnVerify.setBackgroundResource(R.drawable.round_blue);
 
         new CountDownTimer(resendTime * 1000, 1000) {
 
@@ -136,6 +160,7 @@ public class OTPVerificationDialog extends Dialog {
                 resendEnabled = true;
                 tvResend.setText("Gửi lại mã");
                 tvResend.setTextColor(getContext().getResources().getColor(android.R.color.holo_blue_dark));
+                btnVerify.setBackgroundResource(R.drawable.round_back_brown);
             }
         }.start();
     }
@@ -143,7 +168,13 @@ public class OTPVerificationDialog extends Dialog {
     @Override
     public boolean onKeyUp(int keyCode, @NonNull KeyEvent event) {
         if(keyCode == KeyEvent.KEYCODE_DEL) {
-            if(selected == 3) {
+            if(selected == 5) {
+                selected = 4;
+                showKeyBoard(otp5);
+            } else if(selected == 4) {
+                selected = 3;
+                showKeyBoard(otp4);
+            } else if(selected == 3) {
                 selected = 2;
                 showKeyBoard(otp3);
             } else if(selected == 2) {
